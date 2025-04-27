e48ab06456800ded35a0e54b5498678426bacd5c
Vasyl Sidnenko <vasyl.sidenenko@nixs.com>
2025-04-27 18:30:27 +0300

    upd main

--- mcp_server/mcp_main.py
+++ mcp_server/mcp_main.py
@@ -1,8 +1,12 @@
 import os
-from mcp.mcp_server import app
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("MCP_PORT", 10001))
-    host = os.environ.get("MCP_HOST", "0.0.0.0")
-    uvicorn.run("mcp.mcp_server:app", host=host, port=port, reload=True)
+if os.getenv("FLY_APP_NAME"):  # If FLY_APP_NAME environment variable exists (Fly.io automatically provides it)
+    from mcp.mcp_server import app
+else:
+    from mcp.mcp_server import app
+
+    if __name__ == "__main__":
+        import uvicorn
+        port = int(os.environ.get("MCP_PORT", 10001))
+        host = os.environ.get("MCP_HOST", "0.0.0.0")
+        uvicorn.run("mcp.mcp_server:app", host=host, port=port, reload=True)
